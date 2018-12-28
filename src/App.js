04fae9e793febc05a2279eec0b9e9ef538cb398a
Chris Blakely <chrisblakely@Chriss-MacBook-Pro.local>
2018-12-28 18:18:08 +0000

    testing touch backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment} from 'react';
 import Grid from './Grid.js';
 import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
 import { DragDropContextProvider  } from 'react-dnd';
 
 class App extends Component {
   render() {
     return (
-		<DragDropContextProvider backend={HTML5Backend}>
+		<DragDropContextProvider backend={TouchBackend}>
 			<Fragment>
 				<Grid/>
 			</Fragment>
